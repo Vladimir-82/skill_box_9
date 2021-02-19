dbaabb950537abb05ab801cade37fe065f66ccbe
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-02-19 15:44:10 +0300

    two began

---
+++ photo_script.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+import os, time, shutil
+
+# Нужно написать скрипт для упорядочивания фотографий (вообще любых файлов)
+# Скрипт должен разложить файлы из одной папки по годам и месяцам в другую.
+# Например, так:
+#   исходная папка
+#       icons/cat.jpg
+#       icons/man.jpg
+#       icons/new_year_01.jpg
+#   результирующая папка
+#       icons_by_year/2018/05/cat.jpg
+#       icons_by_year/2018/05/man.jpg
+#       icons_by_year/2017/12/new_year_01.jpg
+#
+# Входные параметры основной функции: папка для сканирования, целевая папка.
+# Имена файлов в процессе работы скрипта не менять, год и месяц взять из времени создания файла.
+# Обработчик файлов делать в обьектном стиле - на классах.
+#
+# Файлы для работы взять из архива icons.zip - раззиповать проводником в папку icons перед написанием кода.
+# Имя целевой папки - icons_by_year (тогда она не попадет в коммит)
+#
+# Пригодятся функции:
+#   os.walk
+#   os.path.dirname
+#   os.path.join
+#   os.path.normpath
+#   os.path.getmtime
+#   time.gmtime
+#   os.makedirs
+#   shutil.copy2
+#
+# Чтение документации/гугла по функциям - приветствуется. Как и поиск альтернативных вариантов :)
+# Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+
+# TODO здесь ваш код
+path = 'e:\хлам\python\photo_py'
+path_exit = 'e:\хлам\python\photo_py_exit'
+path_normalized = os.path.normpath(path)
+# dir_exit = os.makedirs(path_exit)
+new_dir_names_list = []
+for dirpath, filepath, filenames in os.walk(path_normalized):
+    for file in filenames:
+        full_file_path = os.path.join(dirpath, file)
+        secs = os.path.getmtime(full_file_path)
+        file_time = time.gmtime(secs)
+        new_dir_name = file_time[0]
+        new_dir_names_list.append(new_dir_name)
+        new_dir_path = path_exit + '\\' + str(new_dir_name)
+
+        os.makedirs(new_dir_path)
+        # print(full_file_path, file_time)
+        shutil.copy2(full_file_path, new_dir_path)
+
+
+# Усложненное задание (делать по желанию)
+# Нужно обрабатывать zip-файл, содержащий фотографии, без предварительного извлечения файлов в папку.
+# Основная функция должна брать параметром имя zip-файла и имя целевой папки.
+# Для этого пригодится шаблон проектирование "Шаблонный метод" см https://goo.gl/Vz4828
